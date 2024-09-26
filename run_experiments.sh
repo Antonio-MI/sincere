c9b70199e9561d80d83841b8e63e87242741e713
Antonio Martinez Ibarra <antonio.martinez@um.es>
2024-09-26 15:50:19 -0400

    sh loop for all parameters

--- run_experiments.sh
+++ run_experiments.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 # Run time for calls
-run_duration=1200
+run_duration=120
 # Timeout duration (run of calls + 60 seconds)
-timeout_duration=1260
-# Distribution followed by input calls
-distribution=bursty # gamma, bursty, ramp
-# Scheduling mode
-mode="SelectBatch+Timer" # One of [BestBatch, BestBatch+Timer, SelectBatch+Timer, BestBatch+PartialBatch+Timer]
+timeout_duration=150
 # Models
 models="granite-7b,gemma-7b,llama3-8b"
-# SLA
-batch_time_limit=45
 
 # Function to check if Flask API is up
 wait_for_flask() {
@@ -23,32 +17,106 @@ wait_for_flask() {
   echo "Flask API is ready!"
 }
 
-# Iterate over traffic_mean values
-for traffic_mean in 4; do
-  echo "Running experiment with traffic_mean = $traffic_mean"
+# Arrays of variables to iterate over
+traffic_means=(4 8)
+distributions=("gamma" "bursty" "ramp")
+modes=("BestBatch" "BestBatch+Timer" "SelectBatch+Timer" "BestBatch+PartialBatch+Timer")
+batch_time_limits=(40 60)
 
-  # Start the Flask API in the background
-  echo "Starting Flask API"
-  timeout $timeout_duration python3 api_scheduler_experiments.py $mode $models $batch_time_limit $distribution $run_duration $traffic_mean &
 
-  # Wait until it is running
-  wait_for_flask
+# Iterate over batch_time_limits
+for batch_time_limit in "${batch_time_limits[@]}"; do
+  echo "Running experiments with batch_time_limit = $batch_time_limit"
+  # Iterate over traffic_mean values
+  for traffic_mean in "${traffic_means[@]}"; do
+    echo "Running experiment with traffic_mean = $traffic_mean"
+    # Iterate over distributions
+    for distribution in "${distributions[@]}"; do
+      echo "Running experiments for distribution: $distribution" 
+      # Iterate over modes
+      for mode in "${modes[@]}"; do
+        echo "Running experiments for mode: $mode"
 
-  # Start the GPU utilization monitoring script in the background
-  echo "Starting GPU utilization monitoring"
-  timeout $timeout_duration bash monitor_gpu.sh &
+        # Start the Flask API in the background
+        echo "Starting Flask API"
+        timeout $timeout_duration python3 api_scheduler_experiments.py "$mode" "$models" $batch_time_limit $distribution $run_duration $traffic_mean &
 
-  # Start the inference script in the background
-  echo "Starting api calls script with traffic_mean = $traffic_mean"
-  timeout $timeout_duration python3 api_calls.py $run_duration $distribution $traffic_mean $models &
+        # Wait until it is running
+        wait_for_flask
 
-  # Wait for all background processes to complete
-  wait
+        # Start the GPU utilization monitoring script in the background
+        echo "Starting GPU utilization monitoring"
+        timeout $timeout_duration bash monitor_gpu.sh &
 
-  echo "All processes for traffic_mean = $traffic_mean have completed."
+        # Start the inference script in the background
+        echo "Starting API calls script with traffic_mean = $traffic_mean"
+        timeout $timeout_duration python3 api_calls.py $run_duration $distribution $traffic_mean "$models" &
+
+        # Wait for all background processes to complete
+        wait
+
+        echo "All processes for traffic_mean = $traffic_mean, batch_time_limit = $batch_time_limit, distribution = $distribution, mode = $mode have completed."
+      done
+    done
+  done
 done
 
-echo "All experiments with different traffic_mean values are done."
+echo "All experiments are done."
+
+
+# #!/bin/bash
+
+# # Run time for calls
+# run_duration=1200
+# # Timeout duration (run of calls + 60 seconds)
+# timeout_duration=1260
+# # Distribution followed by input calls
+# distribution=bursty # gamma, bursty, ramp
+# # Scheduling mode
+# mode="SelectBatch+Timer" # One of [BestBatch, BestBatch+Timer, SelectBatch+Timer, BestBatch+PartialBatch+Timer]
+# # Models
+# models="granite-7b,gemma-7b,llama3-8b"
+# # SLA
+# batch_time_limit=40 # Set two SLAs 40 - 80s
+
+# # Set quick runs for the whole grid of parameters
+
+# # Function to check if Flask API is up
+# wait_for_flask() {
+#   echo "Checking if Flask API is ready..."
+#   while ! curl -s http://127.0.0.1:5000/health >/dev/null; do
+#     echo "Waiting for Flask API to be ready"
+#     sleep 4
+#   done
+#   echo "Flask API is ready!"
+# }
+
+# # Iterate over traffic_mean values
+# for traffic_mean in 4 8; do
+#   echo "Running experiment with traffic_mean = $traffic_mean"
+
+#   # Start the Flask API in the background
+#   echo "Starting Flask API"
+#   timeout $timeout_duration python3 api_scheduler_experiments.py $mode $models $batch_time_limit $distribution $run_duration $traffic_mean &
+
+#   # Wait until it is running
+#   wait_for_flask
+
+#   # Start the GPU utilization monitoring script in the background
+#   echo "Starting GPU utilization monitoring"
+#   timeout $timeout_duration bash monitor_gpu.sh &
+
+#   # Start the inference script in the background
+#   echo "Starting api calls script with traffic_mean = $traffic_mean"
+#   timeout $timeout_duration python3 api_calls.py $run_duration $distribution $traffic_mean $models &
+
+#   # Wait for all background processes to complete
+#   wait
+
+#   echo "All processes for traffic_mean = $traffic_mean have completed."
+# done
+
+# echo "All experiments with different traffic_mean values are done."
 
 ##########################################
 
